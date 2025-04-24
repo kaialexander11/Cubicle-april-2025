2307102b54e4789c911bef24cb05c283444b9c25
Alexander Uzunov <kaialexander.11@gmail.com>
2025-04-24 11:27:56 +0300

    Extract routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,22 @@
 // console.log('Hello from Express!');
 
 const express = require('express');
-const handlebars = require('express-handlebars');
-const path = require('path');
+//const handlebars = require('express-handlebars');
+//const path = require('path');
 
 const expressConfig = require('./config/expressConfig.js');
 const handlebarsConfig = require('./config/handlebarsConfig.js');
-const homeController = require('./controllers/homeController.js');
-const cubeController = require('./controllers/cubeController.js');
+const routes = require('./routes.js');
+
 
 const app = express();
+
 const PORT = 5000;
 
 expressConfig(app);
 handlebarsConfig(app);
+
+app.use(routes);
 
 // app.get('/', homeController.getHome);
 
@@ -24,12 +27,6 @@ handlebarsConfig(app);
 //app.use(express.static(path.resolve(__dirname, 'public')));
 
 
-app.use(homeController);
-app.use('/cubes', cubeController);
 
-// Doesn't work here! WHY???
-// app.get('*', (req, res) => {
-//     res.redirect('/404');
-// });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
