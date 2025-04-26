2dcd653c5b9e67d3c0c11faa0df9285aa6673bbe
Alexander Uzunov <kaialexander.11@gmail.com>
2025-04-26 19:17:03 +0300

    Show cube and accessories on attach page

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,6 +1,8 @@
-const Accessory = require('../models/Accessory');
-const accessoryManager = require('../managers/accessoryManager.js');
+const Accessory = require('../models/Accessory.js');
 
+//const accessoryManager = require('../managers/accessoryManager.js');
 //exports.create = (accessoryData) => console.log(accessoryData);
+
+exports.getAll = () => Accessory.find();
 
 exports.create = (accessoryData) => Accessory.create(accessoryData);
