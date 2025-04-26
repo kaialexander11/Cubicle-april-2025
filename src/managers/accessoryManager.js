426aac536f7b61d9b0b79a47ab3d32a59fc53977
Alexander Uzunov <kaialexander.11@gmail.com>
2025-04-26 15:50:52 +0300

    Add accessory manager

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,1 +1,5 @@
-exports.create = (accessoryData) => console.log(accessoryData);
+const Accessory = require('../models/Accessory');
+
+//exports.create = (accessoryData) => console.log(accessoryData);
+
+exports.create = (accessoryData) => Accessory.create(accessoryData);
