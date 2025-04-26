67995e6500fe1bf8c988c3b2a980c234ca5a3e5c
Alexander Uzunov <kaialexander.11@gmail.com>
2025-04-26 16:13:39 +0300

    Add accessory to db

--- src/managers/accessoryManager.js
+++ src/managers/accessoryManager.js
@@ -1,4 +1,5 @@
 const Accessory = require('../models/Accessory');
+const accessoryManager = require('../managers/accessoryManager.js');
 
 //exports.create = (accessoryData) => console.log(accessoryData);
 
